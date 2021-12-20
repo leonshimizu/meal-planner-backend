42f724b13911ccbc41c2af1687a5849e1aa5c958
“leonshimizu” <lmshimizu@gmail.com>
2021-12-20 09:06:18 -0800

    added https:// to frontend link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'jolly-wing-ebb9cf.netlify.app/'
+    origins 'localhost:8080', 'https://jolly-wing-ebb9cf.netlify.app/'
 
     resource '*',
       headers: :any,
